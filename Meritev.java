4c5ca555390ad85b12c5c63aae5c5bf8d7518bbb
Iztok Bajcar <ib8548@student.uni-lj.si>
2022-02-25 16:12:22 +0100

    Dodal getterje

--- Meritev.java
+++ Meritev.java
@@ -2,12 +2,20 @@ import java.util.Date;
 
 public class Meritev {  // razred za hranjenje posamezne meritve
 
-    private Date datumCas;  // datum in čas meritve
+    private Date datumCas;  // datum in čas meritve (oboje v isti strukturi, potem pa razdelimo na datum in čas posebej, če bo treba?)
     private float volumen;  // vrednost meritve (voluen)
 
     public Meritev(Date datumCas, float volumen) {
         this.datumCas = datumCas;
         this.volumen = volumen;
+    }
+
+    public Date vrniDatum() {
+        return this.datumCas;
+    }
+
+    public float vrniVolumen() {
+        return this.volumen;
     }
 
 }
