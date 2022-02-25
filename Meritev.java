608ccc6a23ad055356bf4eafa21ccf4588bd15d3
Iztok Bajcar <ib8548@student.uni-lj.si>
2022-02-25 17:00:01 +0100

    Dodal veljavnost

--- Meritev.java
+++ Meritev.java
@@ -4,6 +4,7 @@ public class Meritev {  // razred za hranjenje posamezne meritve
 
     private Date datumCas;  // datum in čas meritve (oboje v isti strukturi, potem pa razdelimo na datum in čas posebej, če bo treba?)
     private float volumen;  // vrednost meritve (voluen)
+    private boolean veljavno = false;  // ali je meritev veljavna
 
     public Meritev(Date datumCas, float volumen) {
         this.datumCas = datumCas;
@@ -16,6 +17,14 @@ public class Meritev {  // razred za hranjenje posamezne meritve
 
     public float vrniVolumen() {
         return this.volumen;
+    }
+
+    public boolean jeVeljavno() {
+        return this.veljavno;
+    }
+
+    public void nastaviVeljavno(boolean veljavno) {
+        this.veljavno = veljavno;
     }
 
 }
