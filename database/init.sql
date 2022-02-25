2c23c3793d32aac04b4639f40c6b35c3dc33e4e5
Robert Samardžija <samardzija.robert@gmail.com>
2022-02-25 20:15:59 +0100

    Initialise database

--- database/init.sql
+++ database/init.sql
@@ -1,11 +1,11 @@
 GRANT ALL PRIVILEGES ON DATABASE aquasystems TO aquasystems;
 
 CREATE TABLE IF NOT EXISTS logs (
+    log_id      SERIAL PRIMARY KEY,
     log_date    DATE,
     log_time    TIME,
     log_value   DOUBLE PRECISION,
-    log_flag    BOOLEAN,
-    PRIMARY KEY (log_date, log_time)
+    log_flag    BOOLEAN
 );
 
 COPY logs(log_date, log_time, log_value) FROM '/usr/src/app/database.txt' (FORMAT CSV, DELIMITER('|'));
