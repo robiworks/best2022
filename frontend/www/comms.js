8aca3381b8e4bbc7dd6b71281e14616256ef1ebb
Iztok Bajcar <ib8548@student.uni-lj.si>
2022-02-26 09:40:08 +0100

    Added API call

--- frontend/www/comms.js
+++ frontend/www/comms.js
@@ -1,6 +1,17 @@
-const URL = "http://api:8080";
+const URL = "http://localhost:8080";
 
+// gets every measurement on the specified date
 const getList = (date) => {
-    fetch(URL + "/list")
-        .then((res) => alert(res));
+    fetch(URL + "/api/list?date=fixme", {
+        method: "GET",
+        mode: "cors"
+    }).then((res) => alert(res));
+}
+
+// gets all measurements, grouped and ordered by date
+const getGrouped = () => {
+    fetch(URL + "/api/grouped", {
+        method: "GET",
+        mode: "cors"
+    }).then((res) => alert(res));
 }
