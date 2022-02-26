e1ee56777cda7e35a6d782c9b18b84c970129372
Iztok Bajcar <ib8548@student.uni-lj.si>
2022-02-26 10:24:29 +0100

    Add

--- frontend/www/comms.js
+++ frontend/www/comms.js
@@ -2,10 +2,10 @@ const URL = "http://localhost:8080";
 
 // gets every measurement on the specified date
 const getList = (date) => {
-    fetch(URL + "/api/list?date=fixme", {
+    fetch(URL + "/api/list?date=" + date, {
         method: "GET",
         mode: "cors"
-    }).then((res) => alert(res));
+    }).then((res) => console.log(res));
 }
 
 // gets all measurements, grouped and ordered by date
@@ -13,5 +13,13 @@ const getGrouped = () => {
     fetch(URL + "/api/grouped", {
         method: "GET",
         mode: "cors"
-    }).then((res) => alert(res));
+    }).then((res) => console.log(res));
+}
+
+// adds a measurement into the database
+const addMeasurement = (date, time, value) => {
+    fetch(URL + "/api/add?date=" + date + "&time=" + time + "&value=" + value, {
+        method: "GET",
+        mode: "cors"
+    }).then((res) => console.log(res));
 }
